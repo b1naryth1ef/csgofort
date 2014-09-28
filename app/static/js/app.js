33004873e1a2941a682714a2689a1067c4b4abd6
Y0l0Swaggings <b1naryth1ef@gmail.com>
2014-09-28 08:32:00 -0500

    Cleanup, top-menu, shorten landing-blurb

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -18,26 +18,9 @@ app.alert = function(msg, type) {
     }))
 }
 
-app.setup_top_menu = function() {
-    $("#top-menu-toggle").click(function() {
-        $("#left-side-navi").fadeIn();
-        // if ($("#top-menu").is(":visible")) {
-        //     $("#top-menu").fadeOut(180);
-        //     $(".page-head").css("top", 0);
-        //     $(".page-head").css("margin-top", 0);
-        // } else {
-        //     $("#top-menu").fadeIn(180);
-        //     $(".page-head").css("top", ($("#top-menu").height() - 5));
-        //     // $(".page-head").css("margin-top", $("#top-menu").height() + $("#main_header").height())
-        // }
-    })
-
-    $(".topclick").click(function() {
-        window.location.href = "http://" + $(this).attr("rel") + CONFIG.DOMAIN
-    })
-}
-
+app.setup = function() {}
 
 app.run = function(route) {
+    app.setup();
     run(route);
 }
